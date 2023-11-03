31c3a3fa59cd39fe770be5fe9941380663004fbf
Pornima_Malbhage <malbhage.pornima15@gmail.com>
2023-11-03 15:53:27 +0530

    My google sheet project

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ const headerContainer = document.querySelector(".header");
 const serialNumbersContainer = document.querySelector(".sno");
 const mainContainer = document.querySelector(".main");
 // A => 65
-
 function createHeaderCells() {
     for (let i = 0; i <= columns; i++) {
         const headerCell = document.createElement("div");
@@ -16,7 +15,6 @@ function createHeaderCells() {
         headerContainer.appendChild(headerCell)
     }
 }
-
 function createSerialNumberCells() {
     for (let i = 1; i <= rows; i++) {
         const snoCell = document.createElement("div");
@@ -25,7 +23,6 @@ function createSerialNumberCells() {
         serialNumbersContainer.appendChild(snoCell);
     }
 }
-
 function createRow(rowNumber) {
     // creates a row and each row will have 26 columns
     const row = document.createElement("div");
@@ -35,23 +32,21 @@ function createRow(rowNumber) {
         cell.className = "main-cell cell";
         cell.contentEditable = true;
         row.appendChild(cell);
-
         cell.id = String.fromCharCode(64 + i) + rowNumber;
 
         // adding focus event for every cell.
         cell.addEventListener("focus", onCellFocus);
+        // add input event as we need to record the inside the input 
+        cell.addEventListener("input", onFormChange);
     }
     mainContainer.appendChild(row);
 }
-
 function buildMainSection() {
     // loop for 100 times
     for (let i = 1; i <= rows; i++) {
         createRow(i);
     }
 }
-
 createHeaderCells();
 createSerialNumberCells();
-
 buildMainSection();
